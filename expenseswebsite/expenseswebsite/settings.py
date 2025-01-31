29f92610540d8bbffa25e2401b2df1c1cb8acd0f
GhilaniYassine <ghilaniyassine11@gmail.com>
2025-01-31 18:27:36 +0100

    faced boostrap problem

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "expenses.apps.ExpensesConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
